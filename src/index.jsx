fadf3a7b3ee6080af382910b2d2849991cc62f67
Massyl Bensaid <massyl.bensaid@renault.com>
2023-06-09 00:25:29 +0200

    valid

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./_App";
+import Visualizer from "./Visualizer";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Visualizer />
   </React.StrictMode>
 );
